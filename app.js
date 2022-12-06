0dd1d3a8cff3ebdc9b1a9524c9621cb45d206e24
Ephraim Becker <emb16@outlook.com>
2022-12-06 10:42:44 -0500

    Added passport.

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const crypto = require('crypto');
+
+const port = 3000;
+
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  db.get('SELECT * FROM users WHERE username = ?', [username], function (err, user) {
+    if (err) { return cb(err); }
+    if (!user) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+    crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+      return cb(null, user);
+    });
+  });
+})
+);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/login',
+  function (req, res, next) {
+    res.render('login');
+  });
+
+app.post('/login/password',
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  function (req, res) {
+    res.redirect('/~' + req.user.username);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
